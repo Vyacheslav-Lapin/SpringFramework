7cdc15641248bb1e4405eb6afdfcf7279288c655
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-11-08 13:43:13 +0300

    lab 5 start

--- helloWorld/src/test/java/SimpleAppTest.java
+++ helloWorld/src/test/java/SimpleAppTest.java
@@ -1,52 +1,20 @@
-import lab.model.Country;
 import lab.model.Person;
-import lab.model.UsualPerson;
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.Arrays;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.GenericGroovyApplicationContext;
 
 import static org.junit.Assert.assertEquals;
 
 public class SimpleAppTest {
+	
+//	private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "application-context.xml";
+//	private BeanFactory context = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
+	private BeanFactory context =
+		new GenericGroovyApplicationContext("classpath:beans.groovy");
+	private Person expectedPerson = TestUtils.getExpectedPerson();
 
-    private static final String APPLICATION_CONTEXT_XML_FILE_NAME =
-            "application-context.xml";
-
-    private AbstractApplicationContext context;
-
-    private Person expectedPerson;
-
-    @Before
-    public void setUp() throws Exception {
-        context = new ClassPathXmlApplicationContext(
-                APPLICATION_CONTEXT_XML_FILE_NAME);
-        expectedPerson = getExpectedPerson();
-    }
-
-    @Test
-    public void testInitPerson() {
-        Person person = context.getBean(Person.class);
-        System.out.println(person);
-        assertEquals(expectedPerson, person);
-    }
-
-    private Person getExpectedPerson() {
-        return new UsualPerson()
-                .setAge(35)
-                .setHeight(1.78F)
-                .setProgrammer(true)
-                .setName("John Smith")
-                .setCountry(new Country()
-                        .setId(1)
-                        .setName("Russia")
-                        .setCodeName("RU"))
-                .setContacts(
-                        Arrays.asList(
-                                "asd@asd.ru",
-                                "+7-234-456-67-89")
-                );
-    }
+	@Test
+	public void testInitPerson() {
+		assertEquals(expectedPerson, context.getBean("person", Person.class));
+	}
 }
--- helloWorld/src/test/java/SpringTCFAppTest.java
+++ helloWorld/src/test/java/SpringTCFAppTest.java
@@ -1,51 +1,26 @@
-import lab.model.Country;
 import lab.model.Person;
-import lab.model.UsualPerson;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import configuration.HelloWorldConfig;
 
-import java.util.Arrays;
+import javax.annotation.Resource;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:application-context.xml")
+//@ContextConfiguration("classpath:application-context.xml")
+@ContextConfiguration(classes = HelloWorldConfig.class)
 public class SpringTCFAppTest {
 	
-	@Autowired
-	private Person person1;
-	private Person expectedPerson;
+	@Resource
+	private Person person;
 
-	@Before
-	public void setUp() throws Exception {
-		expectedPerson = getExpectedPerson();
-	}
+	private Person expectedPerson = TestUtils.getExpectedPerson();
 
 	@Test
 	public void testInitPerson() {
-		assertEquals(expectedPerson, person1);
-		System.out.println(person1);
-	}
-
-	private UsualPerson getExpectedPerson() {
-		return new UsualPerson()
-				.setAge(35)
-				.setHeight(1.78F)
-				.setProgrammer(true)
-				.setName("John Smith")
-				.setCountry(new Country()
-						.setId(1)
-						.setName("Russia")
-						.setCodeName("RU"))
-				.setContacts(
-						Arrays.asList(
-								"asd@asd.ru",
-								"+7-234-456-67-89")
-				);
+		assertEquals(expectedPerson, person);
 	}
 }
